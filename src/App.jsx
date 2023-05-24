e35b723f0f7d4bf07f6064c7bccad189ba837e04
zilenmodi <zilen.m@simformsolutions.com>
2023-05-24 16:00:42 +0530

    Add env var in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import MainPageContainer from './containers/MainPageContainer/MainPageContainer'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import MainPageContainer from "./containers/MainPageContainer/MainPageContainer";
 
 function App() {
-
   return (
     <>
+      <h3 style={{ textAlign: "center" }}>{import.meta.env.VITE_APP_MODE}</h3>
       <MainPageContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
